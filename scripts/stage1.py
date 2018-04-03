b04fb9211bfece18bec48f42b7963b7464d2810f
James Ko <jamesqko@gmail.com>
2018-04-03 06:30:57 -0400

    More refactoring

--- scripts/stage1.py
+++ scripts/stage1.py
@@ -19,63 +19,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from urllib.parse import parse_qs, urlparse
 
+# Formats as %m/%d/%Y, but does not leave leading zeroes on the month or day.
+# Surprisingly, the syntax for this is different across platforms: https://stackoverflow.com/a/2073189/4077294
+DATE_FORMAT = '%#m/%#d/%Y' if platform.system() == 'Windows' else '%-m/%-d/%Y'
+
 MESSAGE_NO_INCIDENTS_AVAILABLE = 'There are currently no incidents available.'
-
-def _uuid_is_present(driver, _):
-    form_wrapper = driver.find_element_or_wait(By.CSS_SELECTOR, '.filter-outer.form-wrapper')
-    # TODO: It's possible that the element could become stale in between these calls.
-    form_wrapper_id = None
-    while form_wrapper_id is None:
-        try:
-            form_wrapper_id = form_wrapper.get_attribute('id')
-        except StaleElementReferenceException:
-            # Remember, this code is being run while we're waiting for the form wrapper to be updated.
-            # There's a real chance that update could happen in between the find_element_or_wait and get_attribute calls.
-            continue
-    return 'new' not in form_wrapper_id
-
-def _fmt_date(date):
-    # Format as %m/%d/%Y, but do not leave leading zeroes on the month or day.
-    # Surprisingly, the syntax for this is different across platforms: https://stackoverflow.com/a/2073189/4077294
-    fmt_string = '%#m/%#d/%Y' if platform.system() == 'Windows' else '%-m/%-d/%Y'
-    return date.strftime(fmt_string)
-
-def _get_info(driver, tr):
-    tds = driver.find_elements_or_wait(By.CSS_SELECTOR, 'td', ancestor=tr)
-    assert len(tds) == 7
-    date, state, city_or_county, address, \
-        n_killed, n_injured = map(driver.get_value, tds[:6])
-    n_killed, n_injured = map(int, [n_killed, n_injured])
-
-    incident_a = driver.find_element_or_wait(By.LINK_TEXT, 'View Incident', ancestor=tds[6])
-    incident_url = incident_a.get_attribute('href')
-    try:
-        source_a = driver.find_element_or_wait(By.LINK_TEXT, 'View Source', ancestor=tds[6])
-        source_url = source_a.get_attribute('href')
-    except NoSuchElementException:
-        source_url = ''
-
-    return date, state, city_or_county, address, \
-        n_killed, n_injured, \
-        incident_url, source_url
-
-def main():
-    args = parse_args()
-    log.basicConfig(level=args.log_level)
-    driver = Chrome()
-
-    step = timedelta(days=1)
-    global_start, global_end = dateparser.parse(args.start_date), dateparser.parse(args.end_date)
-    start, end = global_start, global_start + step - timedelta(days=1)
-
-    with open(args.output_file, 'w', encoding='utf-8') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(['date', 'state', 'city_or_county', 'address', 'n_killed', 'n_injured', 'incident_url', 'source_url'])
-
-        while start <= global_end:
-            query(driver, start, end)
-            process_batch(driver, writer)
-            start, end = end + timedelta(days=1), min(global_end, end + step)
 
 def parse_args():
     parts = sys.argv[1].split('-')
@@ -122,9 +70,44 @@ def parse_args():
         args.output_file = 'stage1.{:02d}.{:04d}.csv'.format(month, year)
     return args
 
+def get_info(driver, tr):
+    tds = driver.find_elements_or_wait(By.CSS_SELECTOR, 'td', ancestor=tr)
+    assert len(tds) == 7
+    date, state, city_or_county, address, n_killed, n_injured = map(driver.get_value, tds[:6])
+    n_killed, n_injured = map(int, [n_killed, n_injured])
+
+    incident_a = driver.find_element_or_wait(By.LINK_TEXT, 'View Incident', ancestor=tds[6])
+    incident_url = incident_a.get_attribute('href')
+
+    try:
+        source_a = driver.find_element_or_wait(By.LINK_TEXT, 'View Source', ancestor=tds[6])
+        source_url = source_a.get_attribute('href')
+    except NoSuchElementException:
+        source_url = ''
+
+    return date, state, city_or_county, address, n_killed, n_injured, incident_url, source_url
+
+def main():
+    args = parse_args()
+    log.basicConfig(level=args.log_level)
+    driver = Chrome()
+
+    step = timedelta(days=1)
+    global_start, global_end = dateparser.parse(args.start_date), dateparser.parse(args.end_date)
+    start, end = global_start, global_start + step - timedelta(days=1)
+
+    with open(args.output_file, 'w', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['date', 'state', 'city_or_county', 'address', 'n_killed', 'n_injured', 'incident_url', 'source_url'])
+
+        while start <= global_end:
+            query(driver, start, end)
+            process_batch(driver, writer)
+            start, end = end + timedelta(days=1), min(global_end, end + step)
+
 
 def query(driver, start_date, end_date):
-    log.debug("Querying incidents between {:%m/%d/%Y} and {:%m/%d/%Y}".format(start_date, end_date))
+    print("Querying incidents between {:%m/%d/%Y} and {:%m/%d/%Y}".format(start_date, end_date))
 
     url = 'http://www.gunviolencearchive.org/query'
     print('GET', url)
@@ -136,7 +119,20 @@ def query(driver, start_date, end_date):
     date_link = driver.find_element_or_wait(By.LINK_TEXT, 'Date')
     driver.click(date_link)
 
-    wait = WebDriverWait(driver, timeout=15)
+    def _uuid_is_present(driver, _):
+        form_wrapper = driver.find_element_or_wait(By.CSS_SELECTOR, '.filter-outer.form-wrapper')
+        # TODO: It's possible that the element could become stale in between these calls.
+        form_wrapper_id = None
+        while form_wrapper_id is None:
+            try:
+                form_wrapper_id = form_wrapper.get_attribute('id')
+            except StaleElementReferenceException:
+                # Remember, this code is being run while we're waiting for the form wrapper to be updated.
+                # There's a real chance that update could happen in between the find_element_or_wait and get_attribute calls.
+                continue
+        return 'new' not in form_wrapper_id
+
+    wait = WebDriverWait(driver, timeout=10)
     predicate = partial(_uuid_is_present, driver)
     wait.until(predicate)
 
@@ -147,9 +143,8 @@ def query(driver, start_date, end_date):
 
     input_date_from_id = 'edit-query-filters-{}-outer-filter-filter-field-date-from'.format(uuid)
     input_date_to_id = 'edit-query-filters-{}-outer-filter-filter-field-date-to'.format(uuid)
-    # TODO: This format string will not work on Unix. Use - instead of #
-    start_date_str = _fmt_date(start_date)
-    end_date_str = _fmt_date(end_date)
+    start_date_str = start_date.strftime(DATE_FORMAT)
+    end_date_str = end_date.strftime(DATE_FORMAT)
 
     # HACK HACK HACK
     script = '''
@@ -204,7 +199,7 @@ def process_batch(driver, writer):
 def process_page(driver, writer):
     trs = driver.find_elements_or_wait(By.CSS_SELECTOR, '.responsive .odd, .responsive .even')
     trs = reversed(trs) # Order by ascending date instead of descending
-    infos = map(partial(_get_info, driver), trs)
+    infos = map(partial(get_info, driver), trs)
     for info in infos:
         writer.writerow([*info])
 
