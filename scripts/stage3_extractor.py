3f20770739b94dfb61e21852b84f2ef0dfc52bb9
James Ko <jamesqko@gmail.com>
2018-04-06 15:40:20 -0400

    It finally works?

--- scripts/stage3_extractor.py
+++ scripts/stage3_extractor.py
@@ -30,17 +30,33 @@ def _find_div_with_title(title, soup):
 def _out_name(in_name, prefix=''):
     return prefix + in_name.lower().replace(' ', '_') # e.g. 'Age Group' -> 'participant_age_group'
 
-def _getgroups(lines):
-    # TODO: Enforce that all groups have the same number of values, or do something.
-    groups = defaultdict(list)
+def _getdict(lines, apply=None):
+    d = {}
     for line in lines:
         if not line:
             continue
         index = line.find(':')
         assert index != -1
+
         key, value = line[:index], line[index + 2:]
-        groups[key].append(value)
-    return groups
+        assert key not in d
+        if apply:
+            value = apply(value)
+        d[key] = value
+    return d
+
+def _getdicts(linegroups, apply=None):
+    ds = {}
+    groupno = 0
+    for lines in linegroups:
+        ds[groupno] = _getdict(lines, apply)
+        groupno += 1
+    # Swap the order of mappings. ds[0]['name'] -> ds['name'][0]
+    ds2 = defaultdict(dict)
+    for groupno, d in ds.items():
+        for key, value in d.items():
+            ds2[key][groupno] = value
+    return ds2
 
 def _normalize(fields, all_field_names):
     fields = list(fields)
@@ -66,10 +82,20 @@ def _normalize(fields, all_field_names):
 
     return fields
 
-def _stringify_list(lst, sep):
-    violation = next((sep in item for item in lst), None)
+def _stringify_list(lst, sep='|'):
+    violation = next((item for item in lst if sep in item), None)
     assert violation is None, "List item {} contains the separator character {}".format(repr(violation), repr(sep))
     return sep.join(lst)
+
+def _stringify_dict(d, insep=':', outsep='|'):
+    keys, values = list(map(str, d.keys())), list(map(str, d.values()))
+    key_violation = next((key for key in keys if insep in key or outsep in key), None)
+    value_violation = next((value for value in values if insep in value or outsep in value), None)
+    assert key_violation is None and value_violation is None, \
+           "Key {} or value {} contains the separator character(s) {} or {}".format(
+               repr(key_violation), repr(value_violation), repr(insep), repr(outsep))
+
+    return outsep.join([insep.join([k, v]) for k, v in zip(keys, values)])
 
 class Stage3Extractor(object):
     def __init__(self):
@@ -127,15 +153,15 @@ class Stage3Extractor(object):
         if div is None:
             return
 
-        lines = [li.text for li in div.select('li')]
-        for field_name, field_values in _getgroups(lines).items():
+        linegroups = [[li.text for li in ul.select('li')] for ul in div.select('ul')]
+        for field_name, field_values in _getdicts(linegroups).items():
             field_name = _out_name(field_name, prefix='participant_')
-            field_values = _stringify_list(field_values, sep=',')
+            field_values = _stringify_dict(field_values)
             yield Field(field_name, field_values)
 
     def _extract_incident_characteristics(self, soup):
         div = _find_div_with_title('Incident Characteristics', soup)
-        return None if div is None else _stringify_list([li.text for li in div.select('li')], sep='\n')
+        return None if div is None else _stringify_list([li.text for li in div.select('li')])
 
     def _extract_notes(self, soup):
         div = _find_div_with_title('Notes', soup)
@@ -154,10 +180,10 @@ class Stage3Extractor(object):
         yield Field('n_guns_involved', n_guns_involved)
 
         # List attributes
-        lines = [li.text for li in div.select('li')]
-        for field_name, field_values in _getgroups(lines).items():
+        linegroups = [[li.text for li in ul.select('li')] for ul in div.select('ul')]
+        for field_name, field_values in _getdicts(linegroups).items():
             field_name = _out_name(field_name, prefix='gun_')
-            field_values = _stringify_list(field_values, sep=',')
+            field_values = _stringify_dict(field_values)
             yield Field(field_name, field_values)
 
     def _extract_sources(self, soup):
@@ -174,7 +200,5 @@ class Stage3Extractor(object):
         # Fortunately, each important line is followed by a <br> element, so we can use that to our advantage.
         # NB: The orphaned text elements are of type 'NavigableString'
         lines = [str(br.previousSibling).strip() for br in div.select('br')]
-        for key, values in _getgroups(lines).items():
-            assert len(values) == 1 # It would be strange if the incident took place in more than 1 congressional district
-            value = int(values[0])
+        for key, value in _getdict(lines, apply=int).items():
             yield Field(_out_name(key), value)
