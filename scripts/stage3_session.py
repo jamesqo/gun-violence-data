c93bc3f2eeda8a0bc7bba5715066967bafb136dc
James Ko <jamesqko@gmail.com>
2018-04-06 17:56:06 -0400

    Retry requests in Stage3Session

--- scripts/stage3_session.py
+++ scripts/stage3_session.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 
 from aiohttp import ClientSession
@@ -19,11 +20,36 @@ class Stage3Session(object):
     async def __aexit__(self, type, value, tb):
         await self._sess.__aexit__(type, value, tb)
 
+    def _log_failed_request(self, url):
+        print("ERROR! Failed GET request to {}".format(url), file=sys.stderr)
+
+    def _log_retry(self, url, status, retry_wait):
+        print("GET request to {} failed with status {}. Trying again in {}s...".format(url, status, retry_wait), file=sys.stderr)
+
+    def _log_extraction_failed(self, url):
+        print("ERROR! Extractor failed for the following webpage: {}".format(url), file=sys.stderr)
+
+    async def _get(self, url, retry_limit=5, retry_wait=10):
+        resp = await self._sess.get(url)
+        status = resp.status
+        if retry_limit == 1 or status < 400:
+            return resp
+        elif 400 <= status < 500:
+            self._log_failed_request(url)
+            return resp
+
+        # Server error, try again.
+        async with resp: # Dispose of the response immediately.
+            pass
+        self._log_retry(url, resp.status, retry_wait)
+        await asyncio.sleep(retry_wait)
+        return await self._get(url, retry_limit - 1, retry_wait)
+
     async def get_fields_from_incident_url(self, row):
         incident_url = row['incident_url']
-        async with self._sess.get(incident_url) as resp:
+        resp = await self._get(incident_url)
+        async with resp:
             if resp.status >= 400:
-                print("ERROR! Failed GET request to {}".format(incident_url), file=sys.stderr)
                 resp.raise_for_status()
 
             ctype = resp.headers.get(CONTENT_TYPE, '').lower()
@@ -39,5 +65,5 @@ class Stage3Session(object):
         try:
             return self._extractor.extract_fields(text, ctx)
         except Exception:
-            print("ERROR! Extractor failed for the following webpage: {}".format(incident_url), file=sys.stderr)
+            self._log_extraction_failed(incident_url)
             raise
